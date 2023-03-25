30b4bba7b89cdba0c6483deb27627e1fa424688c
Sadanand Modak <sadanandm@utexas.edu>
2023-03-25 06:04:50 -0500

    update result_dir

--- run_multiple_runs.sh
+++ run_multiple_runs.sh
@@ -6,7 +6,7 @@ for j in $(eval echo {$1..$2})
 do
     for i in $(eval echo {$3..$4})
     do
-        python run.py --algo $5 --param $6 --result_dir "/robodata/smodak/BARN2023/result_$5_$6" --world_idx $i --run_idx $j --gui 0
+        python run.py --algo $5 --param $6 --result_dir "result_$5_$6" --world_idx $i --run_idx $j --gui 0
         pkill -9 python
         pkill -9 rosmaster
         pkill -9 gzclient
