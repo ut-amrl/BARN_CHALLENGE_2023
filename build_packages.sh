eca679a0ade4ded5d95ed6469593241bbc84359d
zichao <zichao@utexas.edu>
2023-10-30 21:18:56 -0500

    add amrl msgs

--- build_packages.sh
+++ build_packages.sh
@@ -4,6 +4,8 @@ ut_jackal_path=$(realpath third_party/ut_jackal)
 echo $ut_jackal_path
 graph_nav_path=$(realpath third_party/ut_jackal/graph_navigation)
 echo $graph_nav_path
+amrl_msgs_path=$(realpath third_party/amrl_msgs)
+echo $amrl_msgs_path
 
 if [[ $ROS_PACKAGE_PATH == *"ut_jackal"* ]]; then
     echo "Removing ut_jackal from ROS_PACKAGE_PATH..."
@@ -25,6 +27,16 @@ if [[ $ROS_PACKAGE_PATH != *"$graph_nav_path"* ]]; then
     export ROS_PACKAGE_PATH=$graph_nav_path:$ROS_PACKAGE_PATH
 fi
 
+if [[ $ROS_PACKAGE_PATH == *"amrl_msgs"* ]]; then
+    echo "Removing amrl_msgs from ROS_PACKAGE_PATH..."
+    export ROS_PACKAGE_PATH=$(echo $ROS_PACKAGE_PATH | tr ':' '\n' | grep -v "amrl_msgs" | paste -sd: -)
+fi
+# Add the new path to ROS_PACKAGE_PATH
+if [[ $ROS_PACKAGE_PATH != *"$amrl_msgs_path"* ]]; then
+    echo "Adding $amrl_msgs_path to ROS_PACKAGE_PATH..."
+    export ROS_PACKAGE_PATH=$amrl_msgs_path:$ROS_PACKAGE_PATH
+fi
+
 cd src/jackal_helper/script
 chmod +x *.sh
 find . -name "*.py" -exec chmod +x {} \;
@@ -33,10 +45,10 @@ cd -
 git submodule update --recursive --init
 # export ROS_PACKAGE_PATH=`pwd`:$ROS_PACKAGE_PATH
 
-#cd third_party/amrl_msgs
-#make -j12 
+cd third_party/amrl_msgs
+make -j12 
 
-cd third_party/ut_jackal
+cd ../ut_jackal
 make -j12
 
 cd ../voronoi_global_planner
